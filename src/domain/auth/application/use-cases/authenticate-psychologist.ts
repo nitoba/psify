f64adbe1e05185a96924d2ff75015c9e0f04e91b
Bruno Alves <nito.dev@gmail.com>
2024-02-25 18:15:08 -0300

    feat: authenticate psychologist

---
+++ src/domain/auth/application/use-cases/authenticate-psychologist.ts
@@ -0,0 +1,54 @@
+import { Either, left, right } from '@/core/either'
+import { InvalidCredentials } from '@/core/errors/use-cases/invalid-credentials'
+
+import { Encrypter } from '../cryptography/encrypter'
+import { HashComparer } from '../cryptography/hash-comparer'
+import { AuthPsychologistRepository } from '../repositories/auth-psychologist-repository'
+
+type AuthenticatePsychologistUseCaseRequest = {
+  email: string
+  password: string
+}
+
+type AuthenticatePsychologistUseCaseResponse = Either<
+  InvalidCredentials,
+  {
+    accessToken: string
+  }
+>
+
+export class AuthenticatePsychologistUseCase {
+  constructor(
+    private readonly authPsychologistRepository: AuthPsychologistRepository,
+    private readonly hasher: HashComparer,
+    private readonly encrypter: Encrypter,
+  ) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticatePsychologistUseCaseRequest): Promise<AuthenticatePsychologistUseCaseResponse> {
+    const psychologist =
+      await this.authPsychologistRepository.findByEmail(email)
+
+    if (!psychologist) {
+      return left(new InvalidCredentials())
+    }
+
+    const isValidPassword = await this.hasher.compare(
+      password,
+      psychologist.password,
+    )
+
+    if (!isValidPassword) {
+      return left(new InvalidCredentials())
+    }
+
+    const accessToken = await this.encrypter.encrypt({
+      sub: psychologist.id.toString(),
+      email: psychologist.email,
+    })
+
+    return right({ accessToken })
+  }
+}
