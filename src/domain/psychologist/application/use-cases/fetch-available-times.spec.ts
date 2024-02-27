b7730ad3a422d96975c8a21f60e1a18d2533e68d
Bruno Alves <nito.dev@gmail.com>
2024-02-27 17:20:11 -0300

    feat: implementing fetch available times

---
+++ src/domain/psychologist/application/use-cases/fetch-available-times.spec.ts
@@ -0,0 +1,99 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Time } from '@/domain/core/enterprise/value-objects/time'
+import { makePsychologist } from '@/test/factories/psychologist/make-psychologist'
+import { InMemoryPsychologistRepository } from '@/test/repositories/psychologist/in-memory-psychologist-repository'
+
+import { AvailableTime } from '../../enterprise/entities/available-time'
+import { AvailableTimesList } from '../../enterprise/entities/available-times-list'
+import { FetchAvailableTimesUseCase } from './fetch-available-times'
+
+describe('FetchAvailableTimesUseCase', () => {
+  let useCase: FetchAvailableTimesUseCase
+  let repository: InMemoryPsychologistRepository
+
+  beforeEach(() => {
+    repository = new InMemoryPsychologistRepository()
+    useCase = new FetchAvailableTimesUseCase(repository)
+  })
+
+  afterEach(() => {
+    vi.clearAllTimers()
+  })
+
+  it('should return available times for a psychologist', async () => {
+    vi.useFakeTimers().setSystemTime(new Date(2024, 1, 25, 8))
+    const psychologistId = new UniqueEntityID()
+    const availableTimes = [
+      AvailableTime.create({
+        psychologistId,
+        weekday: 0,
+        time: Time.create('09:00').value as Time,
+      }),
+      AvailableTime.create({
+        psychologistId,
+        weekday: 1,
+        time: Time.create('12:00').value as Time,
+      }),
+    ]
+    const psychologist = makePsychologist(
+      {
+        availableTimes: new AvailableTimesList(availableTimes),
+      },
+      psychologistId,
+    )
+
+    repository.create(psychologist)
+
+    const result = await useCase.execute({
+      psychologistId: psychologist.id.toString(),
+    })
+
+    expect(result.isRight()).toBeTruthy()
+
+    if (result.isRight()) {
+      expect(result.value.availableTimes).toEqual(availableTimes)
+    }
+  })
+
+  it('should filter available times in the past', async () => {
+    vi.useFakeTimers().setSystemTime(new Date(2024, 1, 25, 10))
+    const psychologistId = new UniqueEntityID()
+    const availableTimes = [
+      AvailableTime.create({
+        psychologistId,
+        weekday: 0,
+        time: Time.create('12:00').value as Time,
+      }),
+      AvailableTime.create({
+        psychologistId,
+        weekday: 0,
+        time: Time.create('09:00').value as Time,
+      }),
+    ]
+    const psychologist = makePsychologist(
+      {
+        availableTimes: new AvailableTimesList(availableTimes),
+      },
+      psychologistId,
+    )
+
+    repository.create(psychologist)
+
+    const result = await useCase.execute({
+      psychologistId: psychologist.id.toString(),
+    })
+
+    expect(result.isRight()).toBeTruthy()
+    if (result.isRight()) {
+      expect(result.value.availableTimes).toEqual([availableTimes[0]])
+    }
+  })
+
+  it('should return left if psychologist not found', async () => {
+    const result = await useCase.execute({
+      psychologistId: '123',
+    })
+
+    expect(result.isLeft()).toBeTruthy()
+  })
+})
---
+++ src/domain/psychologist/application/use-cases/fetch-available-times.ts
@@ -0,0 +1,51 @@
+import { Either, left, right } from '@/core/either'
+import { ResourceNotFound } from '@/core/errors/use-cases/resource-not-found'
+
+import { AvailableTime } from '../../enterprise/entities/available-time'
+import { PsychologistRepository } from '../repositories/psychology-repository'
+
+type FetchAvailableTimesUseCaseRequest = {
+  psychologistId: string
+}
+
+type FetchAvailableTimesUseCaseResponse = Either<
+  ResourceNotFound,
+  {
+    availableTimes: AvailableTime[]
+  }
+>
+
+export class FetchAvailableTimesUseCase {
+  constructor(
+    private readonly psychologistRepository: PsychologistRepository,
+  ) {}
+
+  async execute({
+    psychologistId,
+  }: FetchAvailableTimesUseCaseRequest): Promise<FetchAvailableTimesUseCaseResponse> {
+    const psychologist =
+      await this.psychologistRepository.findById(psychologistId)
+
+    if (!psychologist) {
+      return left(new ResourceNotFound('Psychologist not found'))
+    }
+
+    const currentAvailableTimes = psychologist.availableTimes.getItems()
+
+    // filter availableTimes to response only times more than current date now
+    const availableTimes = currentAvailableTimes.filter((at) => {
+      const [hourFromTime, minutesFromTime] = at.time.value
+        .split(':')
+        .map(Number)
+
+      const dateToCompare = new Date()
+      dateToCompare.setHours(hourFromTime, minutesFromTime)
+
+      return Date.now() <= dateToCompare.getTime()
+    })
+
+    return right({
+      availableTimes,
+    })
+  }
+}
