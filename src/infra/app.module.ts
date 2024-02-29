179683339b2240852eb460cfdcc98c370a00a8aa
Bruno <bruno@sthorm.io>
2024-02-28 23:59:56 -0300

    chore: setup config env service

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 
+import { envSchema } from './env/env'
 import { HttpModule } from './http/http.module'
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    HttpModule,
+  ],
   controllers: [],
   providers: [],
 })
