648de6b322f7e4e6217dccbce6b7b33690ff3fac
Bruno Alves <nito.dev@gmail.com>
2024-02-29 09:17:59 -0300

    chore: get server port from env

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -5,12 +5,17 @@ import {
 } from '@nestjs/platform-fastify'
 
 import { AppModule } from './app.module'
+import { EnvService } from './env/env.service'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   )
-  await app.listen(3000, '0.0.0.0')
+
+  const configService = app.get(EnvService)
+  const port = configService.get('PORT')
+
+  await app.listen(port, '0.0.0.0')
 }
 bootstrap()
