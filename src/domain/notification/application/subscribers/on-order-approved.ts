5ffb7a071fede257785a58d5412429b8e9a34540
Bruno Alves <nito.dev@gmail.com>
2024-03-04 13:08:30 -0300

    feat: change message for approved order

--- src/domain/notification/application/subscribers/on-order-approved.ts
+++ src/domain/notification/application/subscribers/on-order-approved.ts
@@ -30,7 +30,7 @@ export class OnOrderApproved implements EventHandler {
     await this.sendNotificationUseCase.execute({
       to: patient.email,
       subject: 'Order approved',
-      content: `The order with id ${order.id.toString()} was approved`,
+      content: `The order with id ${order.id.toString()} was approved. Now you are able to process the payment`,
     })
   }
 }
