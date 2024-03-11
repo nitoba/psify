37e188d30d8e6de11d5f4918c46611aa246dc245
Bruno Alves <nito.dev@gmail.com>
2024-03-11 14:37:28 -0300

    feat: improvement filters

--- apps/web/src/app/psychologists/_components/psychologist-filters.tsx
+++ apps/web/src/app/psychologists/_components/psychologist-filters.tsx
@@ -4,15 +4,18 @@ import { Search } from 'lucide-react'
 
 export function PsychologistsListFilter() {
   return (
-    <form className="flex items-center gap-2">
+    <form className="flex-col items-center gap-2">
       <span className="text-sm font-semibold">Filtros:</span>
-      <Input placeholder="Nome" className="h-8 w-auto" />
-      <Input placeholder="Especialidades" className="h-8 w-[320px]" />
-
-      <Button type="submit" variant="secondary">
-        <Search className="mr-2 size-4" />
-        Filtrar resultados
-      </Button>
+      <div className="flex items-center justify-between">
+        <div className="flex gap-2">
+          <Input placeholder="Nome" className="h-8 w-auto" />
+          <Input placeholder="Especialidades" className="h-8 w-auto" />
+        </div>
+        <Button type="submit" variant="secondary">
+          <Search className="mr-2 size-4" />
+          Filtrar resultados
+        </Button>
+      </div>
     </form>
   )
 }
